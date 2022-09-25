d1b85dd09517d1cec06a86985101220a0941bd91
unknown <n.andyvue@outlook.com>
2022-09-24 20:07:52 -0500

    final pushes

---
+++ src/components/footer.js
@@ -0,0 +1,24 @@
+import React from 'react'; 
+
+const imgStyle = {
+    width: '75px',
+    height: '75px',
+    padding: '10px'
+}
+
+const copyStyle = {
+    padding: '10px',
+    fontWeight: 'bold',
+}
+
+function Footer() {
+    return(
+        <div class="footer" id="footer">
+            <a href="https://www.linkedin.com/in/nou-vue-791913236/"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/800px-LinkedIn_logo_initials.png" alt="LinkedIn" style={imgStyle}></img></a>
+            <p style={copyStyle}>© Nou Vue 2022</p>
+            <a href="https://github.com/AndyBoyee604"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="Github" style={imgStyle}></img></a>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/components/resume.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const linkStyle = {
+  color: 'white',
+  padding: '20px'
+}
+
+const padding = {
+  padding: '10px'
+}
+
+const titleStyle = {
+  backgroundColor: 'grey',
+  padding: '20px'
+}
+
+function Resume() {
+  return (
+    <section class="mb-4" style={padding}>
+      {/* <h1 className="resume" style={titleStyle}>RESUME</h1> */}
+
+      <h2 className="skills" style={titleStyle}>FRONT END SKILLS</h2>
+
+      <p>HTML</p>
+      <p>CSS</p>
+      <p>jQuery</p>
+      <p>IndexedDB</p>
+
+      <h2 className="skills" style={titleStyle}>BACK END SKILLS</h2>
+
+      <p>Express.js</p>
+      <p>SQL (mySQL, sequelize)</p>
+      <p>noSQL (mongoDB, mongoose)</p>
+      <p>APIs (RESTful, server-side, third-party)</p>
+
+      <button type="button" class="btn btn-dark"><a href="https://docs.google.com/document/d/126gtEfAtpZ-Tas-d8hHcH9kegh5WEblML34rWzXxZ38/edit?usp=sharing
+            " style={linkStyle}>View Full Resume</a></button>
+
+
+    </section>
+  )
+}
+
+export default Resume;
