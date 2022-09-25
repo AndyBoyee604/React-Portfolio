e5249ac5732dc580cea00ac9bde213e824274941
unknown <n.andyvue@outlook.com>
2022-09-24 21:10:47 -0500

    picture in about

--- src/components/about.js
+++ src/components/about.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import photo from '../images/IMG_2385.jpg'
 
 const imgStyle = {
     padding: '20px'
@@ -12,7 +13,7 @@ function About() {
     return(
         <div class="jumbotron">
   <h1 class="display-4">About Me</h1>
-  <img class="headshot" src="https://github.com/AndyBoyee604/React-Portfolio/blob/main/public/images/IMG_2385.jpg" alt="headshot" style={imgStyle}></img>
+  <img class="headshot" src= {photo}  alt="headshot" style={imgStyle}></img>
   <p class="lead">My name is Nou but you can call me Andy. I've always had an interest in the IT field but never did pursue a degree or certificate relate to IT. Reason being is in the IT field there is so many different careers to choose from but after a few research I decided I would like to become a coder. I have a few friends who are already in it and some that are just starting as well. Which is one of the reason that got me interested in coding but it's also an interesting one, like how just typing in codes can make how a website look and operate. Though I'm sure coding is not a simple thing to learn, I'm sure that after my coding bootcamp I would have learned more than enough to kick start my career in coding and am super excited for this!</p>
   <hr class="my-4"></hr>
   <p></p>
