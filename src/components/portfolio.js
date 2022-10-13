9529ae23beb066ace4563ff655599e10f27bdeaa
AndyBoyee604 <n.andyvue@outlook.com>
2022-10-13 15:09:43 -0500

    updated resume

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -17,13 +17,12 @@ function Wrapper(props) {
     return <div className="wrapper" style={padding}>{props.children}</div>
 }
 
-function Portfolio () {
-    return(
+function Portfolio() {
+    return (
         <section>
-
             <Wrapper id="project-data">
                 {portfolio.map((project) => (
-                    <ProjectCards key={project.id} image={project.image} name={project.name} github={project.github} deploy={project.deploy} technologies={project.technologies}/>
+                    <ProjectCards key={project.id} image={project.image} name={project.name} github={project.github} deploy={project.deploy} technologies={project.technologies} />
                 ))}
             </Wrapper>
         </section>
